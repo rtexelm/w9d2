71892bec289158658aeb633959b90bd805de5739
“Catherine <cathanders10@gmail.com>
2022-11-08 17:30:33 -0500

    got something to show up in browser

--- browser_tic_tac/src/index.js
+++ browser_tic_tac/src/index.js
@@ -2,10 +2,14 @@ const View = require("./ttt-view.js")
 const Game = require("/ttt_node/game.js")
 
 document.addEventListener("DOMContentLoaded", () => {
-  new View().setup
-  new Game()
+  
+  const newGame = new Game();
 
-  const view = document.querySelector(".ttt")
+  const view = document.querySelector(".ttt");
+
+  const newView = new View(newGame, view);
+
+  
   
 });
 
--- browser_tic_tac/src/ttt-view.js
+++ browser_tic_tac/src/ttt-view.js
@@ -1,38 +1,58 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+
+    this.setupBoard();
+
+  }
 
   setupBoard() {
     const board = document.createElement('ul')
-    for (let i = 0; i <= 9; i++){
-      // let space = document.createElement('li')
-      let li = document.createElement('li')
-      board.appendChild(li)
-    }
 
     let rowCount = 0;
     let colCount = 0;
 
-    for (let i = 0; i < board.childNodes.length; i++) {
+    for (let i = 0; i < 9; i++){
 
-      if (i === 2|| i === 5) rowCount++
-      if (i === 2 || i === 5) colCount = 0;
+      if (i === 3 || i === 6) rowCount++
+      if (i === 3 || i === 6) colCount = 0;
 
-      board.childNodes[i].createAttribute('data-row')
-      board.childNodes[i].setAttribute('data-row', rowCount)
-
-      board.childNodes[i].createAttribute('data-column')
-      board.childNodes[i].setAttribute('data-column', colCount)
+      let li = document.createElement('li')
+      li.dataset.pos = JSON.stringify([rowCount,colCount]);
+      board.appendChild(li)
 
       colCount++
     }
 
-    document.querySelector("ul").style.display = 'flex';
-    document.querySelector("ul").style.width = '900px';
-    document.querySelector("ul").style.flexWrap = 'wrap';
-    document.querySelector("li").style.width = "300";
-    document.querySelector("li").style.border = "4px";
-    document.querySelector("li").style.backgroundColor = "grey";
-    document.querySelector("li").style.backgroundColor = "grey";
+    this.el.appendChild(board);
+
+
+
+    // for (let i = 0; i < board.childNodes.length; i++) {
+
+    //   let currChild = board.childNodes[i]
+
+    //   if (i === 2|| i === 5) rowCount++
+    //   if (i === 2 || i === 5) colCount = 0;
+
+      
+    // }
+
+
+    // const dataRow = document.createAttribute('data-row')
+    // currChild.setAttribute(dataRow, rowCount)
+    // debugger
+    // board.childNodes[i].createAttribute('data-column')
+    // board.childNodes[i].setAttribute('data-column', colCount)
+
+    // document.querySelector("ul").style.display = 'flex';
+    // document.querySelector("ul").style.width = '900px';
+    // document.querySelector("ul").style.flexWrap = 'wrap';
+    // document.querySelector("li").style.width = "300px";
+    // document.querySelector("li").style.border = "4px";
+    // document.querySelector("li").style.backgroundColor = "grey";
+    // document.querySelector("li").style.backgroundColor = "grey";
 
 
     // board.childNodes.forEach(li => {
